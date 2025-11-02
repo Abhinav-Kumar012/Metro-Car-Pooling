5e3681ab2372dd3b1c775150220e6d999fe2719d
valmikGit <quantumcomputingiiitb@gmail.com>
2025-11-02 14:58:34 +0530

    all services initialized

---
+++ notification/src/test/java/com/metrocarpool/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metrocarpool.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
