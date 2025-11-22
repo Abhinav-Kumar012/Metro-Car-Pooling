c278769a959e226b49d2406923ee0166fab25a3e
KrishDave1 <Krish.Dave@iiitb.ac.in>
2025-11-22 17:04:36 +0530

    Timestamp format changed to ISO for frontend integration

--- gateway/src/main/java/com/metrocarpool/gateway/dto/RiderStatusResponseDTO.java
+++ gateway/src/main/java/com/metrocarpool/gateway/dto/RiderStatusResponseDTO.java
@@ -1,8 +1,13 @@
 package com.metrocarpool.gateway.dto;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Builder
+@Getter
+@Setter
 public class RiderStatusResponseDTO {
     private Boolean status;
+    private String arrivalTime;
 }
